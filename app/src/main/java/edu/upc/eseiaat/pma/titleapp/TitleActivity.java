c81a366807bd7a1baa0e6c939efdc66c9e3c8bd6
Marta <martabarrachina1@hotmail.com>
2017-10-16 15:38:48 +0200

    Obrim l'activitat EditTitleActivity per poder editar el text de TitleActivity però encara no funciona

--- app/src/main/java/edu/upc/eseiaat/pma/titleapp/TitleActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/titleapp/TitleActivity.java
@@ -1,7 +1,9 @@
 package edu.upc.eseiaat.pma.titleapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class TitleActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class TitleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
+    }
+
+    public void editTitle(View view){
+
+        // crear un 'intent'
+        //objecte de la classe intent
+
+        Intent intent = new Intent(this, EditTitleActivity.class ); // this és un punter a l'activitat actual, Vull obrir una activitat de aquesta classe ( editTitleActivity)
+        // 2. Passa l'intetn a Android perquè obri l'activitat
+        startActivity(intent); //
+
+
     }
 }
