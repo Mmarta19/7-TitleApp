62a2371cb4261e33a579b0bf23a1ac82c7f09659
Marta <martabarrachina1@hotmail.com>
2017-10-16 16:12:49 +0200

    Hem fet que la segona activitat envies les dades i es modifiqués a la primera activitat

--- app/src/main/java/edu/upc/eseiaat/pma/titleapp/EditTitleActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/titleapp/EditTitleActivity.java
@@ -1,13 +1,44 @@
 package edu.upc.eseiaat.pma.titleapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class EditTitleActivity extends AppCompatActivity {
+
+    //atribut
+    private EditText edit_title;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_title);
+
+        //(II)
+        // cridem l'intent del Titleactivity
+        Intent intent = getIntent();
+        // Agafem el valor que té title a l'altre activitat
+        String title = intent.getStringExtra("title");
+        edit_title = (EditText)findViewById(R.id.edit_title);
+        // li posem al edit Text d'aquesta acitivat
+        edit_title.setText(title);
+
+    }
+
+    // enviar el que hi ha a l'activitat anterior
+
+    public void saveTitle(View view) {
+        // (III)
+        String new_title=edit_title.getText().toString();
+        Intent data = new Intent();
+        data.putExtra("title", new_title);
+
+        setResult(RESULT_OK, data); // li passem les dades com a resultat ( és com un return)
+
+        // tancar l'activitat actual i tornar a la de abans
+        finish();
     }
 }
--- app/src/main/java/edu/upc/eseiaat/pma/titleapp/TitleActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/titleapp/TitleActivity.java
@@ -1,26 +1,60 @@
 package edu.upc.eseiaat.pma.titleapp;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class TitleActivity extends AppCompatActivity {
+
+    private String title="Aquest és el títol inicial...";
+    // atribut per poder-hi accedir
+    private TextView title_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
+
+        title_text = (TextView)findViewById(R.id.title);
+        title_text.setText(title);
     }
 
     public void editTitle(View view){
 
-        // crear un 'intent'
+       //  (I)
+
+        // 1. crear un 'intent'
         //objecte de la classe intent
 
         Intent intent = new Intent(this, EditTitleActivity.class ); // this és un punter a l'activitat actual, Vull obrir una activitat de aquesta classe ( editTitleActivity)
-        // 2. Passa l'intetn a Android perquè obri l'activitat
-        startActivity(intent); //
+        // 2. Afegir paràmetres ( dades extra) a la crida de l'activitat
+        intent.putExtra("title", title);
+        // 3. Passa l'intent a Android perquè obri l'activitat
+
+       //  startActivity(intent); no l'utilitzem
+        // cridem aquest mètode perquè l'activitat anterior ens torna algo
+        startActivityForResult(intent,0);
+    }
+
+    // mètode perquè té un result
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // requestCode ha de ser el mateix que el de abans
+        // esultCode és el RESULT_OK/CANCEL...
+        // data són les dades que retorna l'altre activitat
+
+        switch(requestCode){
+            //(IV)
+            case 0: // activitat quan has posat un 0 en el startActivityForResult
+                if(resultCode == AppCompatActivity.RESULT_OK){
+                    title = data.getStringExtra("title");
+                    title_text.setText(title);
+                }
+
+        }
 
 
     }
